0b9de65a263581b4b9e329511a969995e49205d6
Anthony64M <anthony.mutuku@student.moringaschool.com>
2021-09-24 21:44:23 +0300

    Links after SqlAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,12 @@ from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_uploads import IMAGES, UploadSet,configure_uploads
+
+db = SQLAlchemy()
+mail = Mail()
+bootstap = Bootstrap()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
+
