3cbc3be9c96404a11c8cdced302678c930536f05
Anthony64M <anthony.mutuku@student.moringaschool.com>
2021-09-24 22:27:57 +0300

    Imported required files

---
+++ app/main/views.py
@@ -0,0 +1,12 @@
+from flask import render_template,redirect,url_for,abort,request,flash
+from app.main import main
+from app.models import User,Blog,Comment,Subscriber
+from .forms import UpdateProfile,CreateBlog
+from .. import db
+from app.requests import get_quotes
+from flask_login import login_required,current_user
+from ..email import mail_message
+import secrets
+import os
+from PIL import Image
+
