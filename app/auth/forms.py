da3e3cb12a6939883deb1516deeb57cbb161f86d
Anthony64M <anthony.mutuku@student.moringaschool.com>
2021-09-24 21:34:23 +0300

    Validation after input

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -16,3 +16,12 @@ class RegForm(FlaskForm):
     password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
     password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
     submit = SubmitField('Sign Up')
+
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError(message="Sorry Email assigned user")
+    
+    def validate_username(self, data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError(message="Sorry username assigned to another user")
