36085af39ee8a8afb27b38e8180d729713506590
Anthony64M <anthony.mutuku@student.moringaschool.com>
2021-09-24 21:24:14 +0300

    Imported required files

---
+++ app/auth/forms.py
@@ -0,0 +1,5 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,BooleanField
+from wtforms.validators import Required,Email,EqualTo, Length
+from wtforms import ValidationError
+from ..models import User
