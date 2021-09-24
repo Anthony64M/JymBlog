478940a1cca4d1360e838aeaf4b1bdae85f2d8fd
Anthony64M <anthony.mutuku@student.moringaschool.com>
2021-09-24 21:52:56 +0300

    Set pass,and verify pass.

--- app/models.py
+++ app/models.py
@@ -17,3 +17,26 @@ class User (UserMixin,db.Model):
     hashed_password = db.Column(db.String(255),nullable = False)
     blog = db.relationship('Blog', backref='user', lazy='dynamic')
     comment = db.relationship('Comment', backref='user', lazy='dynamic')
+
+
+    @property
+    def set_password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @set_password.setter
+    def password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.hashed_password,password)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return "User: %s" %str(self.username)
