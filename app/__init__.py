d4b707b9b3ae47d10abf82d717b1ad36917630a7
Anthony64M <anthony.mutuku@student.moringaschool.com>
2021-09-27 09:41:42 +0300

    Added several  files

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,15 @@
-from flask import Flask 
-from config import Config
+from flask import Flask
+from config import config_options
 from flask_mail import Mail
 from flask_login import LoginManager
-from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap, bootstrap_find_resource
 from flask_sqlalchemy import SQLAlchemy
 from flask_uploads import IMAGES, UploadSet,configure_uploads
 
+
 db = SQLAlchemy()
 mail = Mail()
-bootstap = Bootstrap()
+bootstrap = Bootstrap()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
@@ -16,10 +17,9 @@ photos = UploadSet('photos',IMAGES)
 
 
 
-
-def create_app():
+def create_app(config_name):
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config_options[config_name])
     from .auth import auth as authentication_blueprint
     from .main import main as main_blueprint
 
@@ -28,8 +28,8 @@ def create_app():
 
     login_manager.init_app(app)
     db.init_app(app)
-    bootstap.init_app(app)
+    bootstrap.init_app(app)
     configure_uploads(app,photos)
     mail.init_app(app)
-
+    
     return app
